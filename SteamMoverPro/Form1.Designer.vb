6e5b47cac2df26f8b058c6379747ea88de4e8d4c
ajc5869 <ajc5869@rit.edu>
2016-07-28 10:31:18 -0400

    Init commit
    Init commit

---
+++ SteamMoverPro/Form1.Designer.vb
@@ -0,0 +1,233 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Path1 = New System.Windows.Forms.TextBox()
+        Me.Path2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.FolderBrowserDialog2 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Path1
+        '
+        Me.Path1.Location = New System.Drawing.Point(15, 14)
+        Me.Path1.Name = "Path1"
+        Me.Path1.Size = New System.Drawing.Size(206, 20)
+        Me.Path1.TabIndex = 0
+        '
+        'Path2
+        '
+        Me.Path2.Location = New System.Drawing.Point(360, 13)
+        Me.Path2.Name = "Path2"
+        Me.Path2.Size = New System.Drawing.Size(217, 20)
+        Me.Path2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(224, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(39, 20)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(583, 13)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(39, 20)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(269, 194)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(85, 50)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "------>"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(269, 250)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(85, 50)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "<------"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(269, 466)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(85, 50)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "Start"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.Location = New System.Drawing.Point(12, 57)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(251, 459)
+        Me.ListView1.TabIndex = 20
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Game Folder"
+        Me.ColumnHeader1.Width = 170
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Size (GB)"
+        '
+        'ListView2
+        '
+        Me.ListView2.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView2.FullRowSelect = True
+        Me.ListView2.Location = New System.Drawing.Point(360, 57)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(253, 459)
+        Me.ListView2.TabIndex = 21
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        Me.ListView2.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Game Folder"
+        Me.ColumnHeader3.Width = 170
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Size (GB)"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 22
+        Me.Label1.Text = "Label1"
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(357, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 23
+        Me.Label2.Text = "Label2"
+        Me.Label2.Visible = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 541)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(601, 26)
+        Me.ProgressBar1.TabIndex = 24
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 525)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(128, 13)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "Operation x/x: abc --> xyz"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 250
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(625, 579)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListView2)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Path2)
+        Me.Controls.Add(Me.Path1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "SteamMover Pro"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Path1 As TextBox
+    Friend WithEvents Path2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents FolderBrowserDialog2 As FolderBrowserDialog
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ListView2 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ SteamMoverPro/Form1.vb
@@ -0,0 +1,399 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Public Path1toPath2 As New ArrayList
+    Public Path2toPath1 As New ArrayList
+    Public gigabyte_size As New Single
+
+    Public transfer_to_folder_size As New Double
+    Public transfer_from_folder_size As New Double
+    Public bleh As New Double
+    Public TRANSFER_COLOR As Color = Color.SkyBlue
+    Public BG_COLOR As Color = Color.White
+    Public itemsToMove As New ArrayList
+    Public pathToCheck As String
+    Dim timerThread As System.Threading.Thread
+    Public full_command As String
+    Dim copy_thread As System.Threading.Thread
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Pops up the folder select dialog box
+        FolderBrowserDialog1.ShowNewFolderButton = False
+        FolderBrowserDialog1.ShowDialog()
+
+        If FolderBrowserDialog1.SelectedPath = "" Then
+
+        Else
+            Path1.Text = FolderBrowserDialog1.SelectedPath
+
+            Path1toPath2.Clear()
+            ListView1.Items.Clear()
+
+            'Adds Path1 folder contents to the first listbox
+            For Each folder As String In System.IO.Directory.GetDirectories(Path1.Text)
+                Dim attrs As FileAttributes
+                Dim dInfo As New DirectoryInfo(folder)
+                If attrs And FileAttributes.ReparsePoint Then
+                Else
+
+                    folder = folder.Remove(0, Len(Path1.Text) + 1)
+                    Dim lvi As New ListViewItem
+                    lvi.Text = folder
+
+                    ' set bool parameter to false if you
+                    ' do not want to include subdirectories.
+                    Dim sizeOfDir As Long = DirectorySize(dInfo, True)
+
+                    gigabyte_size = sizeOfDir / (1024 ^ 3)
+                    lvi.SubItems.Add(Math.Round(gigabyte_size, 2).ToString)
+
+                    ListView1.Items.Add(lvi)
+                End If
+            Next
+
+            Dim drive_letter As System.IO.DriveInfo
+            drive_letter = My.Computer.FileSystem.GetDriveInfo(Path1.Text.Substring(0, 3))
+            Label1.Text = CStr(Math.Round((drive_letter.TotalFreeSpace) / (1024 ^ 3), 2)) + " GB Free"
+            Label1.Visible = True
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Pops up the folder select dialog box
+        FolderBrowserDialog2.ShowNewFolderButton = False
+        FolderBrowserDialog2.ShowDialog()
+
+        If FolderBrowserDialog2.SelectedPath = "" Then
+
+        Else
+            Path2.Text = FolderBrowserDialog2.SelectedPath
+
+            Path2toPath1.Clear()
+            ListView2.Items.Clear()
+
+            'Adds Path2 folder contents to the second listbox
+            For Each folder As String In System.IO.Directory.GetDirectories(Path2.Text)
+                Dim attrs As FileAttributes
+                Dim dInfo As New DirectoryInfo(folder)
+                If attrs And FileAttributes.ReparsePoint Then
+                Else
+                    folder = folder.Remove(0, Len(Path2.Text) + 1)
+                    Dim lvi As New ListViewItem(folder)
+
+
+                    ' set bool parameter to false if you
+                    ' do not want to include subdirectories.
+                    Dim sizeOfDir As Long = DirectorySize(dInfo, True)
+
+                    gigabyte_size = sizeOfDir / (1024 ^ 3)
+                    lvi.SubItems.Add(Math.Round(gigabyte_size, 2).ToString)
+                    ListView2.Items.Add(lvi)
+                End If
+            Next
+
+            Dim drive_letter As System.IO.DriveInfo
+            drive_letter = My.Computer.FileSystem.GetDriveInfo(Path2.Text.Substring(0, 3))
+            Label2.Text = CStr(Math.Round((drive_letter.TotalFreeSpace) / (1024 ^ 3), 2)) + " GB Free"
+            Label2.Visible = True
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim settingWhite As Integer = 0
+        If String.IsNullOrWhiteSpace(Path2.Text) Or String.IsNullOrWhiteSpace(Path1.Text) Then
+            MessageBox.Show("Need to specify source and destination path", "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+
+            Dim SelectedItem As New ListViewItem
+            For Each SelectedItem In ListView1.SelectedItems
+                Dim thingy As New ListViewItem
+                thingy.Text = SelectedItem.Text
+                thingy.SubItems.Add(SelectedItem.SubItems(1).Text)
+                ListView2.Items.Add(thingy)
+                If SelectedItem.BackColor = TRANSFER_COLOR Then
+                    thingy.BackColor = BG_COLOR
+                    Path2toPath1.Remove(Path2.Text + "\" + SelectedItem.Text)
+                Else
+                    thingy.BackColor = TRANSFER_COLOR
+                    Path1toPath2.Add(Path1.Text + "\" + SelectedItem.Text)
+                End If
+
+                ListView1.Items.Remove(SelectedItem)
+
+
+
+
+            Next
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        If String.IsNullOrWhiteSpace(Path2.Text) Or String.IsNullOrWhiteSpace(Path1.Text) Then
+            MessageBox.Show("Need to specify source and destination path", "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            Dim itemsToMove As New ArrayList
+
+            Dim SelectedItem As New ListViewItem
+            For Each SelectedItem In ListView2.SelectedItems
+
+                itemsToMove.Add(SelectedItem.Text)
+
+                Dim thingy As New ListViewItem
+                thingy.Text = SelectedItem.Text
+                thingy.SubItems.Add(SelectedItem.SubItems(1).Text)
+                If SelectedItem.BackColor = TRANSFER_COLOR Then
+                    thingy.BackColor = Color.White
+                    Path1toPath2.Remove(Path1.Text + "\" + SelectedItem.Text)
+                Else SelectedItem.BackColor = Color.White
+                    Path2toPath1.Add(Path2.Text + "\" + SelectedItem.Text)
+                    thingy.BackColor = TRANSFER_COLOR
+                End If
+                ListView1.Items.Add(thingy)
+
+                ListView2.Items.Remove(SelectedItem)
+            Next
+
+        End If
+    End Sub
+
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        If Path1toPath2.Count = 0 And Path2toPath1.Count = 0 Then
+            MessageBox.Show("There is nothing to transfer", "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            Dim totalNumber As Integer = Path1toPath2.Count() + Path2toPath1.Count()
+            Dim count As Integer = 0
+
+            MsgBox(totalNumber)
+
+
+            transfer_to_folder_size = 0
+            transfer_from_folder_size = 0
+
+            Dim dInfo As New DirectoryInfo(Path2.Text)
+            transfer_to_folder_size = DirectorySize(dInfo, True)
+
+
+            For Each item In Path1toPath2
+                Dim dInfo2 As New DirectoryInfo(item)
+                'MsgBox(item)
+                transfer_from_folder_size += DirectorySize(dInfo2, True)
+            Next
+
+
+
+            Dim dInfo3 As New DirectoryInfo(Path2.Text)
+            transfer_to_folder_size = DirectorySize(dInfo3, True)
+
+            Dim BlueItem As New ListViewItem
+            For Each BlueItem In ListView1.Items
+                If BlueItem.BackColor = Color.SkyBlue Then
+                    transfer_from_folder_size += BlueItem.SubItems(1).Text
+                End If
+            Next
+
+            For Each item In Path1toPath2
+                count += 1
+                Label3.Text = count.ToString + "/" + totalNumber.ToString + " " + item.Remove(0, Len(Path1.Text) - 1) + " to " + Path2.Text
+
+
+                Dim counter As Integer = 0
+                While counter <= 2
+                    If counter = 0 Then
+                        full_command = " /c xcopy /E /V /I /F /Y " + """" + item + """" + " " + """" + Path2.Text + "\" + item.Remove(0, Len(Path1.Text) + 1) + """"
+                    End If
+
+                    If counter = 1 Then
+                        full_command = " /c rd /S /Q " + """" + item + """"
+                    End If
+
+                    If counter = 2 Then
+                        full_command = " /c mklink /J " + """" + item + """" + " " + """" + Path2.Text + "\" + item.Remove(0, Len(Path1.Text) + 1) + """"
+                    End If
+
+                    Dim CopyStart As ProcessStartInfo = New ProcessStartInfo
+                    Dim start As Process = New Process
+                    CopyStart.CreateNoWindow = True
+                    CopyStart.WindowStyle = ProcessWindowStyle.Hidden
+
+                    CopyStart.FileName = "cmd.exe"
+                    CopyStart.Arguments = full_command
+                    CopyStart.UseShellExecute = False
+                    start = Process.Start(CopyStart)
+                    start.WaitForExit()
+                    counter += 1
+                End While
+            Next
+            Path1toPath2.Clear()
+
+
+
+            For Each item In Path2toPath1
+                count += 1
+                Label3.Text = count.ToString + "/" + totalNumber.ToString + " " + item.Remove(0, Len(Path2.Text) - 1) + " to " + Path1.Text
+
+                Dim counter As Integer = 0
+                While counter <= 2
+                    If counter = 0 Then
+                        full_command = " /c xcopy /E /V /I /F /Y " + """" + item + """" + " " + """" + Path1.Text + "\" + item.Remove(0, Len(Path2.Text) + 1) + """"
+
+                    End If
+
+                    If counter = 1 Then
+                        full_command = " /c rd /S /Q " + """" + item + """"
+                    End If
+
+                    If counter = 2 Then
+                        full_command = " /c mklink /J " + """" + item + """" + " " + """" + Path1.Text + "\" + item.Remove(0, Len(Path2.Text) + 1) + """"
+                    End If
+
+                    Dim CopyStart As ProcessStartInfo = New ProcessStartInfo
+                    Dim start As Process = New Process
+                    CopyStart.CreateNoWindow = True
+                    CopyStart.WindowStyle = ProcessWindowStyle.Hidden
+
+                    CopyStart.FileName = "cmd.exe"
+                    CopyStart.Arguments = full_command
+                    CopyStart.UseShellExecute = False
+                    start = Process.Start(CopyStart)
+                    start.WaitForExit()
+                    counter += 1
+                End While
+            Next
+            Path2toPath1.Clear()
+        End If
+
+    End Sub
+
+    Private Function DirectorySize(ByVal dInfo As DirectoryInfo,
+   ByVal includeSubDir As Boolean) As Long
+        ' Enumerate all the files
+        Dim totalSize As Long = dInfo.EnumerateFiles() _
+          .Sum(Function(file) file.Length)
+
+        ' If Subdirectories are to be included
+        If includeSubDir Then
+            ' Enumerate all sub-directories
+            totalSize += dInfo.EnumerateDirectories() _
+             .Sum(Function(dir) DirectorySize(dir, True))
+        End If
+        Return totalSize
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListView1.GridLines = True
+        ListView2.GridLines = True
+
+        Path1.Text = My.Settings.Path1
+        Dim driveLetter As String = Mid(Environment.GetFolderPath(Environment.SpecialFolder.System), 1, 3)
+        'Dim pathToCheck As String
+
+        If Environment.Is64BitOperatingSystem = True Then
+            pathToCheck = driveLetter + "Program Files (x86)\Steam\steamapps\common"
+        Else
+            pathToCheck = driveLetter + "Program Files\Steam\steamapps\common"
+        End If
+
+
+        If Directory.Exists(pathToCheck) Then
+            Path1.Text = pathToCheck
+
+            Path1toPath2.Clear()
+            ListView1.Items.Clear()
+
+            'Adds Path1 folder contents to the first listbox
+            For Each folder As String In System.IO.Directory.GetDirectories(Path1.Text)
+                Dim attrs As FileAttributes
+                Dim dInfo As New DirectoryInfo(folder)
+                If attrs And FileAttributes.ReparsePoint Then
+                Else
+                    folder = folder.Remove(0, Len(Path1.Text) + 1)
+                    Dim lvi As New ListViewItem
+                    lvi.Text = folder
+
+                    ' set bool parameter to false if you
+                    ' do not want to include subdirectories.
+                    Dim sizeOfDir As Long = DirectorySize(dInfo, True)
+
+                    gigabyte_size = sizeOfDir / (1024 ^ 3)
+                    lvi.SubItems.Add(Math.Round(gigabyte_size, 2).ToString)
+
+                    ListView1.Items.Add(lvi)
+                End If
+
+            Next
+
+            Dim drive_letter As System.IO.DriveInfo
+            drive_letter = My.Computer.FileSystem.GetDriveInfo(Path1.Text.Substring(0, 3))
+            Label1.Text = CStr(Math.Round((drive_letter.TotalFreeSpace) / (1024 ^ 3), 2)) + " GB Free"
+            Label1.Visible = True
+        Else
+            MsgBox("Steam installation not detected, please manually locate your games")
+
+        End If
+
+    End Sub
+
+
+    Private Sub Form1_FormClosing(sender As Object, e As EventArgs) Handles MyBase.FormClosing
+        My.Settings.Path1 = Path1.Text
+        My.Settings.Path2 = Path2.Text
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs)
+        transfer_to_folder_size = 0
+        transfer_from_folder_size = 0
+
+        Dim dInfo As New DirectoryInfo(Path2.Text)
+        transfer_to_folder_size = DirectorySize(dInfo, True)
+
+
+        For Each item In Path1toPath2
+            Dim dInfo2 As New DirectoryInfo(item)
+            'MsgBox(item)
+            transfer_from_folder_size += DirectorySize(dInfo2, True)
+        Next
+
+        bleh = transfer_from_folder_size
+        MsgBox(bleh)
+        Timer1.Enabled = True
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'MsgBox(bleh)
+        Dim dInfo As New DirectoryInfo(Path2.Text)
+        Dim current_transfer_size As New Double
+        current_transfer_size = DirectorySize(dInfo, True)
+        Dim firstThing As New Integer
+        Dim secondThing As New Integer
+
+        bleh = bleh + 1
+
+        Dim temp As Double
+        firstThing = current_transfer_size - transfer_to_folder_size
+        secondThing = firstThing / bleh
+        temp = secondThing
+        MsgBox(temp)
+        ProgressBar1.Value = temp * 100
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Path1_TextChanged(sender As Object, e As EventArgs) Handles Path1.TextChanged
+
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+
+    End Sub
+End Class
